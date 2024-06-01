7a69658870f5be05c52020ba4cce71386ca23a2f
CAP05090 <ksand7398@gmail.com>
2024-06-01 09:13:53 +0530

    ___

--- src/routes/challengeEditorRoute.js
+++ src/routes/challengeEditorRoute.js
@@ -5,6 +5,7 @@ const editorRouter = express.Router();
 // Function to execute code
 const executeCode = async (language, code, stdin = '') => {
     let command, args;
+
     switch (language.toLowerCase()) {
         case 'python':
             command = 'python';
@@ -33,13 +34,17 @@ const executeCode = async (language, code, stdin = '') => {
         default:
             throw new Error('Unsupported language');
     }
+
     return new Promise((resolve, reject) => {
         const process = spawn(command, args);
         let output = "";
+
         process.stdin.write(stdin);
         process.stdin.end();
+
         process.stdout.on("data", (data) => { output += data.toString(); });
         process.stderr.on('data', (data) => { output += data.toString(); });
+
         process.on('close', (code) => {
             code !== 0 ? reject(output) : resolve(output);
         });
@@ -54,14 +59,16 @@ const runTestCases = async (code, language, testCases) => {
     for (const testCase of testCases) {
         try {
             const output = await executeCode(language, code, testCase.input);
-            outputs.push(output.trim());
+            const trimmedOutput = output.trim();
+            outputs.push(trimmedOutput);
             expectedOutput.push(testCase.output.trim());
-            if (output.trim() !== testCase.output.trim()) allTestsPassed = false;
+            if (trimmedOutput !== testCase.output.trim()) allTestsPassed = false;
         } catch (error) {
             allTestsPassed = false;
             outputs.push(error.toString());
         }
     }
+
     return { allTestsPassed, outputs, expectedOutput };
 };
 
@@ -80,11 +87,13 @@ editorRouter.post('/run', async (req, res) => {
 // Route for submitting code
 editorRouter.post('/submit', async (req, res) => {
     const { code, language, challengeId } = req.body;
+
     try {
         const testCases = [
             { input: "home", output: "emoh" },
             { input: "loohcs", output: "school" },
         ];
+
         const { allTestsPassed, outputs, expectedOutput } = await runTestCases(code, language, testCases);
 
         if (allTestsPassed) {
